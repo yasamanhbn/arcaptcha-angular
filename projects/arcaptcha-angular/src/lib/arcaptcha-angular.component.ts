bb901e19fb647bbd6904e9f0b74d48288b0f8adc
hbn.yasaman <hbn.yasaman@gmail.com>
2022-09-01 17:56:07 +0430

    fixes callbacks error

--- projects/arcaptcha-angular/src/lib/arcaptcha-angular.component.ts
+++ projects/arcaptcha-angular/src/lib/arcaptcha-angular.component.ts
@@ -10,32 +10,35 @@ declare global {
   selector: 'lib-arcaptcha-angular',
   template: '<div id={{id}} href="arcaptcha"></div>',
 })
-
 export class ArcaptchaAngularComponent implements OnInit {
-  constructor() { }
+  constructor() {}
 
-  @Input() callback = function name() { };
+  @Input() public callback :(arg: any)=>void;
+  @Input() public rendered_callback :(arg: any)=>void;
+  @Input() public error_callback :(arg: any)=>void;
+  @Input() public reset_callback :(arg: any)=>void;
+  @Input() public expired_callback :(arg: any)=>void;
+  @Input() public chlexpired_callback :(arg: any)=>void;
   @Input() site_key = '';
   @Input() lang = 'fa';
-  @Input() theme = 'light'
-  @Input() invisible = false
-  @Input() color = 'normal'
+  @Input() theme = 'light';
+  @Input() invisible = false;
+  @Input() color = 'normal';
 
-  @Output() onsetChallengeId: EventEmitter<number> = new EventEmitter();
-
-  id: string = ''
-  widget_id: string = ''
-
+  id: string = '';
+  widget_id: string = '';
 
   ngOnInit(): void {
-    this.setID()
-    var my_script = document.head.querySelector("#arcptcha-script") as HTMLImageElement | null;;
-    const script = my_script || document.createElement("script") ;
+    this.setID();
+    var my_script = document.head.querySelector(
+      '#arcptcha-script'
+    ) as HTMLImageElement | null;
+    const script = my_script || document.createElement('script');
 
     if (!my_script) {
       window.arcaptchaWidgetLoading = new Promise<void>((resolve, reject) => {
         script.src = `https://widget.arcaptcha.ir/1/api.js`;
-        script.id = "arcptcha-script";
+        script.id = 'arcptcha-script';
         script.onload = () => {
           resolve();
           this.initialize();
@@ -54,36 +57,65 @@ export class ArcaptchaAngularComponent implements OnInit {
 
   initialize() {
     this.loadCaptcha();
-    window.addEventListener('arcaptcha-token-changed-' + this.widget_id,
-    event => { /* this.onsetChallengeId.emit((<any>event).detail)  */})
+    window.addEventListener(
+      'arcaptcha-token-changed-' + this.widget_id,
+      (event) => {
+        /* this.onsetChallengeId.emit((<any>event).detail)  */
+      }
+    );
   }
-
+  registerCallback() {
+    if (this.callback){
+      (window as { [key: string]: any })['arcaptcha_callback_' + this.id] =
+        this.callback;
+    }
+    if (this.rendered_callback)
+      (window as { [key: string]: any })[
+        'arcaptcha_rendered_callback_' + this.id
+      ] = this.rendered_callback;
+    if (this.error_callback)
+      (window as { [key: string]: any })[
+        'arcaptcha_error_callback_' + this.id
+      ] = this.error_callback;
+    if (this.reset_callback)
+      (window as { [key: string]: any })[
+        'arcaptcha_reset_callback_' + this.id
+      ] = this.reset_callback;
+    if (this.expired_callback)
+      (window as { [key: string]: any })[
+        'arcaptcha_expired_callback_' + this.id
+      ] = this.expired_callback;
+    if (this.chlexpired_callback)
+      (window as { [key: string]: any })[
+        'arcaptcha_chlexpired_callback_' + this.id
+      ] = this.chlexpired_callback;
+  }
   loadCaptcha() {
-    if (this.callback)
-      (window as { [key: string]: any })['arcaptcha_callback_' + this.id] = this.callback
-
+    this.registerCallback()
     const widgetId = (window as any).arcaptcha.render('#' + this.id, {
-      "site-key": this.site_key,
+      'site-key': this.site_key,
       lang: this.lang,
       theme: this.theme,
-      size: this.invisible ? "invisible" : "",
-      callback: this.invisible
-        ? 'arcaptcha_callback_' + this.id
-        : null,
-      color:this.color
-    })
-    this.widget_id = widgetId
+      size: this.invisible ? 'invisible' : '',
+      callback:'arcaptcha_callback_' + this.id ,
+      rendered_callback:'arcaptcha_rendered_callback_' + this.id ,
+      error_callback:'arcaptcha_error_callback_' + this.id ,
+      reset_callback:'arcaptcha_reset_callback_' + this.id ,
+      expired_callback:'arcaptcha_expired_callback_' + this.id ,
+      chlexpired_callback:'arcaptcha_chlexpired_callback_' + this.id ,
+      color: this.color,
+    });
+    this.widget_id = widgetId;
   }
 
   getRandomID() {
     return (
-      new Date().getTime().toString(36) +
-      Math.random().toString(36).slice(2)
+      new Date().getTime().toString(36) + Math.random().toString(36).slice(2)
     );
   }
 
   setID() {
-    this.id = 'arcaptcha-widget-' + this.getRandomID()
+    this.id = 'arcaptcha-widget-' + this.getRandomID();
   }
 
   resetCaptcha() {
@@ -93,6 +125,4 @@ export class ArcaptchaAngularComponent implements OnInit {
   execute() {
     (window as any).arcaptcha.execute(this.widget_id);
   }
-
 }
-
